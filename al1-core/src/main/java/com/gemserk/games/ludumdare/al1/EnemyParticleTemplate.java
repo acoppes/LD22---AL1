37ef6c47b2982c03683be250cfed355caf8e8c98
Ariel Coppes <ariel.coppes@gemserk.com>
2011-12-17 17:05:57 -0200

    added images to enemy particles too

--- al1-core/src/main/java/com/gemserk/games/ludumdare/al1/EnemyParticleTemplate.java
+++ al1-core/src/main/java/com/gemserk/games/ludumdare/al1/EnemyParticleTemplate.java
@@ -2,23 +2,28 @@ package com.gemserk.games.ludumdare.al1;
 
 import com.artemis.Entity;
 import com.artemis.World;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.gemserk.commons.artemis.components.LinearVelocityLimitComponent;
 import com.gemserk.commons.artemis.components.PhysicsComponent;
+import com.gemserk.commons.artemis.components.RenderableComponent;
 import com.gemserk.commons.artemis.components.ScriptComponent;
 import com.gemserk.commons.artemis.components.SpatialComponent;
+import com.gemserk.commons.artemis.components.SpriteComponent;
 import com.gemserk.commons.artemis.scripts.ScriptJavaImpl;
 import com.gemserk.commons.artemis.templates.EntityTemplateImpl;
 import com.gemserk.commons.gdx.box2d.BodyBuilder;
 import com.gemserk.commons.gdx.games.Spatial;
 import com.gemserk.commons.gdx.games.SpatialPhysicsImpl;
+import com.gemserk.resources.ResourceManager;
 
 public class EnemyParticleTemplate extends EntityTemplateImpl {
 
 	BodyBuilder bodyBuilder;
+	ResourceManager<String> resourceManager;
 
 	public static class FollowMainCharacterScript extends ScriptJavaImpl {
 
@@ -63,12 +68,18 @@ public class EnemyParticleTemplate extends EntityTemplateImpl {
 				.angle(spatial.getAngle()) //
 				.userData(entity) //
 				.build();
+		
+		spatial.setSize(0.5f, 0.5f);
 
 		entity.addComponent(new PhysicsComponent(body));
 		entity.addComponent(new LinearVelocityLimitComponent(MathUtils.random(7f, 15f)));
 		
 		entity.addComponent(new SpatialComponent(new SpatialPhysicsImpl(body, spatial)));
 		entity.addComponent(new ScriptComponent(new FollowMainCharacterScript()));
+		
+		Sprite sprite = resourceManager.getResourceValue(GameResources.Sprites.EnemyParticle);
+		entity.addComponent(new SpriteComponent(sprite));
+		entity.addComponent(new RenderableComponent(0));
 	}
 
 }

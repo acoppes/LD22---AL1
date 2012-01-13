357ec5681af6fc8bfaff009a812e53bb75765c86
Ariel Coppes <ariel.coppes@gemserk.com>
2012-01-13 15:20:51 -0200

    changed stick controller to move the center whenever you touch the screen the first time

--- al1-core/src/main/java/com/gemserk/games/ludumdare/al1/scripts/StickControllerScript.java
+++ al1-core/src/main/java/com/gemserk/games/ludumdare/al1/scripts/StickControllerScript.java
@@ -15,13 +15,13 @@ public class StickControllerScript extends ScriptJavaImpl {
 
 	private final Vector2 tmp = new Vector2();
 	private final Input input;
-	
+
 	int touch = 0;
-	
+
 	public Vector2 stickPosition = new Vector2();
 	public boolean moving = false;
 	public float radius = 80f;
-	
+
 	private ButtonMonitor pointerDownMonitor;
 
 	public StickControllerScript(Input input) {
@@ -33,32 +33,21 @@ public class StickControllerScript extends ScriptJavaImpl {
 	@Override
 	public void update(World world, Entity e) {
 		pointerDownMonitor.update();
-		
+
 		int x = input.getX();
 		int y = Gdx.graphics.getHeight() - input.getY();
 
 		tmp.set(0f, 0f);
 
 		Controller controller = Components.getControllerComponent(e).controller;
-		
+
 		if (pointerDownMonitor.isPressed()) {
-
-			tmp.set(stickPosition.x, stickPosition.y);
-			tmp.sub(x, y);
-
-			if (tmp.len() < radius) {
-				moving = true;
-			} else {
-				tmp.set(0,0);
-			}
-			
+			stickPosition.set(x, y);
+			moving = true;
 		}
-		
+
 		if (pointerDownMonitor.isReleased())
 			moving = false;
-
-		// if (!input.isTouched())
-		// moving = false;
 
 		if (moving) {
 
